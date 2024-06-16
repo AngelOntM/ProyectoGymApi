db73a171c6777604ab3b8c1ba93f85ff3426a1ca
Angel Jahaziel Ontiveros Mendez <94997082+AngelOntM@users.noreply.github.com>
2024-06-15 23:15:26 -0600

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -87,7 +87,7 @@ class AuthController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'email' => 'required|string|email|max:50',
-            'password' => 'required|string|min:6',
+            'password' => 'required|string|min:8',
         ]);
 
         if ($validator->fails()) {
