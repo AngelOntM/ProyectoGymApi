9619588b880ee647d32ed63cc0c0c6f34165e923
Angel Jahaziel Ontiveros Mendez <94997082+AngelOntM@users.noreply.github.com>
2024-07-04 22:10:54 -0600

    Correcion a bd

--- database/migrations/2024_06_15_212621_create_orders_table.php
+++ database/migrations/2024_06_15_212621_create_orders_table.php
@@ -10,8 +10,8 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->nullable();
-            $table->foreignId('employee_id')->constrained('users')->nullable();
+            $table->foreignId('user_id')->nullable()->constrained('users');
+            $table->foreignId('employee_id')->nullable()->constrained('users');
             $table->timestamp('order_date')->useCurrent();
             $table->decimal('total_amount', 10, 2);
             $table->enum('estado', ['Proceso', 'Pagada', 'Cancelada']);
